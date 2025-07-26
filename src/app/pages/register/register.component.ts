192b4e7ee47dc52f4663bb0d3acd7cb106adf4f2
tarekelsayedali <tarek.elsayed@kickstartinteractive.com>
2025-07-26 17:51:23 +0300

    reg Form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
-
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule , InputGroupModule, InputGroupAddonModule, InputTextModule],
   standalone: true,
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  name!: FormControl;
+  email!: FormControl;
+  password!: FormControl;
+  rePassword!: FormControl;
+  registerForm!: FormGroup;
+
+  constructor() {
+    this.initFormControl();
+    this.initFormGroup();
+  }
+
+  initFormControl() {
+    this.name = new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(20)]);
+    this.email = new FormControl("", [Validators.required, Validators.email]);
+    this.password = new FormControl("", [Validators.required]);
+    this.rePassword = new FormControl("", [Validators.required,this.passwordMatch(this.password)]);
+  }
+
+  initFormGroup() {
+    this.registerForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      rePassword: this.rePassword,
+    })
+  }
+  passwordMatch(pass: AbstractControl): ValidatorFn {
+    return (rePass: AbstractControl): null | { [key: string]: boolean } => {
+      if (pass.value !== rePass.value) {
+        return { passNotMatch: true }
+      } else return null;
+    }
+  }
+
+
+
+
+
+}
