0130c5afec53fa857bee584d2cc44993f89e2b23
tarekelsayedali <tarek.elsayed@kickstartinteractive.com>
2025-07-27 19:24:20 +0300

    loginForm

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,108 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { Message, MessageService } from 'primeng/api';
+import { AuthService } from '../../core/service/auth.service';
+import { Ilogin, Iregister } from '../../core/intergaces/iregister';
+import { ToastModule } from 'primeng/toast';
+import { RippleModule } from 'primeng/ripple';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    ButtonModule,
+    MessagesModule,
+    ToastModule,
+    RippleModule,
+    NgxSpinnerModule,
+  ],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
+  providers: [MessageService],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email!: FormControl;
+  password!: FormControl;
+  loginForm!: FormGroup;
+  constructor(
+    private _authService: AuthService,
+    private _messageService: MessageService,
+    private _spinner: NgxSpinnerService,
+    private _router: Router
+  ) {
+    this.initFormControl();
+    this.initFormGroup();
+  }
+
+  initFormControl() {
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]);
+  }
+  initFormGroup() {
+    this.loginForm = new FormGroup({
+      email: this.email,
+      password: this.password,
+    });
+  }
+  submit() {
+    if (this.loginForm.valid) {
+      this.singIn(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+      Object.keys(this.loginForm.controls).forEach((control) => {
+        this.loginForm.controls[control].markAsDirty();
+      });
+    }
+  }
+  singIn(data: Ilogin) {
+    this._spinner.show();
+    this._authService.login(data).subscribe({
+      next: (res) => {
+        if (res._id) {
+          console.log(res);
+
+          this.showToster('success', 'Success', 'Success Login');
+        }
+        this._spinner.hide();
+        this._router.navigate(['user'])
+      },
+      error: (err) => {
+        this._spinner.hide();
+        this.showToster('error', 'Error', err.error.error);
+      },
+    });
+  }
+
+
+  showToster(severity: string, summary: string, detail: string) {
+    this._messageService.add({
+      severity: severity,
+      summary: summary,
+      detail: detail,
+    });
+  }
+}
