cf143219f52fe8954f792aaa0cf8efcd7695cf47
tarekelsayedali <tarek.elsayed@kickstartinteractive.com>
2025-08-08 19:38:48 +0300

    user-Layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { GalleriaModule } from 'primeng/galleria';
+import { CardComponent } from '../../shared/card/card/card.component';
 @Component({
   selector: 'app-home',
-  imports: [GalleriaModule],
+  imports: [GalleriaModule, CardComponent],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
@@ -12,7 +13,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.images = [
-       {
+      {
         itemImageSrc: './assets/product-1.jpg',
         alt: 'Description for Product 1',
         title: 'product 1',
