4540fb87832195ca51eef390e1cd0882bcfab0cf
Tarek El_Sayed <64559653+tarek-elsayed@users.noreply.github.com>
2025-08-09 00:28:05 +0300

    Revert "User layout"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,38 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { GalleriaModule } from 'primeng/galleria';
-import { CardComponent } from '../../shared/card/card/card.component';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-home',
-  imports: [GalleriaModule, CardComponent],
+  imports: [],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit {
-  images: any[] | undefined;
-
-  ngOnInit() {
-    this.images = [
-      {
-        itemImageSrc: './assets/product-1.jpg',
-        alt: 'Description for Product 1',
-        title: 'product 1',
-      },
-      {
-        itemImageSrc: './assets/product-2.jpg',
-        alt: 'Description for Product 1',
-        title: 'product 1',
-      },
-      {
-        itemImageSrc: './assets/product-3.jpg',
-        alt: 'Description for Product 1',
-        title: 'product 1',
-      },
-      {
-        itemImageSrc: './assets/product-4.jpg',
-        alt: 'Description for Product 1',
-        title: 'product 1',
-      },
-    ];
-  }
-}
+export class HomeComponent {}
